e4ae93444aefeffacb0668cecaf8555aa81c6643
jojov <jojov@PAVILION15>
2022-12-01 19:38:15 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.vb
@@ -0,0 +1,12 @@
+﻿Public Class Alumno
+
+    'Declaramos propiedades
+    Private codigo As String
+    Private nombre As String
+    Private dui As String
+    Private genero As String
+    Private edad As String
+    Private correo As String
+    Private carrera As String
+
+End Class
