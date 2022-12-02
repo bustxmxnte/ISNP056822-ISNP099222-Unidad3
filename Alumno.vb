29f86efc763db72ed6e50a653c12be6d740095fe
Jorge Alberto Vasquez Bustamante <ISNP056822@ugb.edu.sv>
2022-12-01 20:59:50 -0600

    se realizo la clase "Alumno" a la que se le agregaron sus métodos correspondientes, a su ves la clase "registro_alumno" a la cual copie el código de ejemplo

--- Alumno.vb
+++ Alumno.vb
@@ -98,4 +98,70 @@
         datos_completos = False
 
     End Sub
+
+
+    'Atributos de clase
+    Public Sub datos_alumnos(ByVal codigo As String,
+                             ByVal nombre As String,
+                             ByVal apellido As String,
+                             ByVal dui As String,
+                             ByVal edad As String,
+                             ByVal correo As String,
+                             ByVal carrera As String
+                            )
+
+        datos_completos = False
+
+
+        If nombre.Length = 0 Then
+            MsgBox("Debe agregar uno o dos nombres")
+        Else
+            nombre = nombre
+        End If
+
+        If apellido.Length = 0 Then
+            MsgBox("Debe agregar uno o dos apellidos")
+        Else
+            apellido = apellido
+        End If
+
+        If dui.Length = 0 Then
+            MsgBox("Debe ingresar un número de DUI")
+        Else
+            dui = dui
+        End If
+
+        If edad.Length = 0 Then
+            MsgBox("Debe agregar un nombre")
+        Else
+            edad = edad
+        End If
+
+        If correo.Length = 0 Then
+            MsgBox("Debe agregar un nombre")
+        Else
+            correo = correo
+        End If
+
+        If carrera.Length = 0 Then
+            MsgBox("Debe agregar un nombre")
+        Else
+            carrera = carrera
+        End If
+
+    End Sub
+
+
+    Public Function generar_codigo()
+
+        Dim valor1 As String
+        Dim valor2 As String
+        Dim numero As Single
+
+        valor1 = UCase(Left(nombre, 1))
+        valor2 = UCase(Left(apellido, 1))
+        numero = Int(Rnd() * 1000) + 77
+
+        Return valor1 & valor2 & numero
+    End Function
 End Class
--- Form.vb
+++ Form.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
